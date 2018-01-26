645a1fae1003deb4bedda1e879b16fc1c77cd13f
narendra <narendra.siripurpu@gmail.com>
2018-01-26 20:01:13 +0530

    add newmessage and create message'

--- server/server.js
+++ server/server.js
@@ -1,10 +1,44 @@
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT||3000;
+const socketIO = require('socket.io');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
- 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+//io.on listens to connection from client.
+io.on('connection', (socket) => {
+    console.log('new user connection');
+
+    socket.emit('newEmail', {
+        from: 'naren@gmail.com',
+        text: 'hey wassup',
+        createAt: 123
+    });
+    
+    socket.emit('newMessage',{
+        from: 'john',
+        text: 'see you then',
+        createAt: 123123
+    });
+
+    socket.on('createMessage',(message)=>{
+
+        console.log('createMessage',message);
+
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`server is up on port ${port}`);
 });
+
